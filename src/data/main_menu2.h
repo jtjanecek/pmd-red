8012a12362a2beed47eeef7d48a3642d1e20c6f8
John Janecek <janecektyler@gmail.com>
2025-09-30 14:35:40 -0600

    add v0.01

--- src/data/main_menu2.h
+++ src/data/main_menu2.h
@@ -160,7 +160,7 @@ static const WindowTemplate sUnknown_80E5CB4 = {
 };
 
 static const MenuItem sUnknown_80E5CCC[] = {
-    {_("Start an entirely new adventure."), 0xFFDE},
+    {_("Red Rescue Team Reborn v0.01"), 0xFFDE},
     {_("  "), 0xFFDE},
     {NULL, 0xFFDD},
 };
@@ -178,8 +178,8 @@ static const MenuItem sUnknown_80E5D48[] = {
 };
 
 static const MenuItem sUnknown_80E5DA0[] = {
-    {_("Resume your adventure from where"), 0xFFDE},
-    {_("you last saved.  "), 0xFFDE},
+    {_("Red Rescue Team Reborn v0.01"), 0xFFDE},
+    {_("  "), 0xFFDE},
     {NULL, 0xFFDD},
 };
 

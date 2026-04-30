c5be79185d9a1769f9ce8ec7f9f617fa627b5b50
doctor-plasmadrid <oficialetalekemon@gmail.com>
2026-04-30 18:59:03 +0200

    Commited workspace using git commands

--- include/rogue_version.h
+++ include/rogue_version.h
@@ -3,7 +3,7 @@
 
 // Global version label for Rogue Rescue Team. Keep this in one place
 // so credits, menus, and other UI can stay in sync.
-#define ROGUE_VERSION_STRING "v1.1.0"
+#define ROGUE_VERSION_STRING "v2.0.0"
 #define ROGUE_VERSION_LABEL "Rogue Rescue Team " ROGUE_VERSION_STRING
 
 #endif // GUARD_ROGUE_VERSION_H

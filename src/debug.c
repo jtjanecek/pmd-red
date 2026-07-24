d99fe0c41d4dd4bd9910d012ae1e1a3f9a8951e9
John Janecek <janecektyler@gmail.com>
2026-07-24 13:18:38 -0700

    remove log thing

--- src/debug.c
+++ src/debug.c
@@ -224,16 +224,8 @@ UNUSED static void UnusedHang(void)
 // arm9.bin::02018964
 void Log(u8 num, const u8 *text, ...)
 {
-    char buf[256];
-    va_list vArgv;
     (void)num;
-
-    va_start(vArgv, text);
-    vsprintf(buf, (const char *)text, vArgv);
-    va_end(vArgv);
-
-    MgbaWarnf("%s", buf);
-    MgbaLog(buf);
+    (void)text;
 }
 
 #if (GAME_VERSION == VERSION_RED)
